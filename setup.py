ad56cc739b76bc7489e841159d2844ddbc10a427
Ravindra Bharathi <ravindrabharathi@users.noreply.github.com>
2019-09-16 15:53:30 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tf_utils',
+      version='0.01',
+      description='utilities for tf2',
+      url='https://github.com/ravindrabharathi/tf_utils',
+      author='ravindrabharathi',
+      author_email='srbharathee@gmail.com',
+      license='MIT',
+      packages=['tf_utils'],
+      zip_safe=False)
