7e7301b1af7cae6b12d046171aa601c7d2da16d7
Ravindra Bharathi <ravindrabharathi@users.noreply.github.com>
2019-10-02 19:46:17 +0530

    Update transform.py

--- tf_utils/transform.py
+++ tf_utils/transform.py
@@ -25,10 +25,13 @@ def flip_left_right(image):
   return tf.image.random_flip_left_right(image)
 
 def cutout(img, prob=100, size=8, min_size=5, use_fixed_size=True):
-  return tf.cond(tf.random.uniform([], 0, 100) > prob, lambda: img , lambda: get_cutout(img,prob,size,min_size,use_fixed_size))
-
-
-
+  num_dim=len(img.get_shape().as_list())
+  if num_dim==4:
+    return tf.cond(tf.random.uniform([], 0, 100) > prob, lambda: img , lambda: get_cutout(img,prob,size,min_size,use_fixed_size))
+  else:
+    return tf.cond(tf.random.uniform([], 0, 100) > prob, lambda: img , lambda: get_cutout_2(img,prob,size,min_size,use_fixed_size))
+  
+#for batch of images 
 def get_cutout(img,prob=50,size=8,min_size=2,use_fixed_size=True):
   
   shp=tf.shape(img)
@@ -56,6 +59,58 @@ def get_cutout(img,prob=50,size=8,min_size=2,use_fixed_size=True):
   img1,
   [0,x1, y1, 0],
   [shp[0],s, s, channel]
+     )
+  
+  
+  #create mask similar in shape to input image with cutout area having ones and rest of the area padded with zeros 
+  mask = tf.image.pad_to_bounding_box(
+    cut_slice,
+    x1,
+    y1,
+    height,
+    width
+  )
+  
+  
+  #invert the zeros and ones 
+  mask = tf.ones_like(mask ) - mask
+  
+  #print(tf.shape(mask))
+  
+  tmp_img = tf.multiply(img,mask)
+  
+  cut_img =tmp_img
+   
+  return cut_img 
+
+#for single image
+def get_cutout_2(img,prob=50,size=8,min_size=2,use_fixed_size=True):
+  
+  shp=tf.shape(img)
+  
+  
+  height = width = tf.shape(img)[1]
+  
+  channel = tf.shape(img)[-1]
+  
+  
+
+  #get cutout size and offsets 
+  if (use_fixed_size==True):
+    s=size
+  else:  
+    s=tf.random.uniform([], min_size, size, tf.int32) # use a cutout size between 5 and size 
+
+  x1 = tf.random.uniform([], 0, height+1-s , tf.int32) # get the x offset from top left
+  y1 = tf.random.uniform([], 0, width+1-s , tf.int32) # get the y offset from top left 
+  
+  # create the cutout slice and the mask 
+  img1 = tf.ones_like(img)  
+  #print(tf.shape(img1))
+  cut_slice = tf.slice(
+  img1,
+  [x1, y1, 0],
+  [s, s, channel]
      )
   
   
