3709a5fa4e6b76d125615724aa25e981d0e2be98
Ravindra Bharathi <ravindrabharathi@users.noreply.github.com>
2020-11-27 10:35:05 +0530

    tfrecords functions specific to deepweeds dataset

--- tf_utils/data.py
+++ tf_utils/data.py
@@ -20,15 +20,19 @@ import psutil
 from tf_utils.utils import *
 from tf_utils.transform import *
 
+from sklearn.model_selection import train_test_split
 
-## for cifar10 
-tar = 'cifar-10-python.tar.gz'
-url = 'https://www.cs.toronto.edu/~kriz/' + tar
 
-num_classes=10
+
+
+
+
+
+num_classes=9
 batch_size=128
-class_names = ['airplane','automobile','bird','cat','deer',
-               'dog','frog','horse','ship','truck']
+class_names = ['0','1','2','3','4','5','6','7','8']
+species_names=class_map.to_dict()['Species']
+#print(species_names)
 
 #import tf if not defined 
 try:
@@ -39,81 +43,24 @@ else:
     pass
 
 
-#for tf2 eager execution is enabled by default 
-# for lower versions enable eager execution
-if (int(str(tf.__version__)[:1])<2):
-  tf.compat.v1.enable_eager_execution()
 
-#get the cpu cores on current env 
-CPU_CORES=get_cpu_num() #get_cpu_cores()  
-
-# features of a single record
+# features of a single record , we could add species names too but for now keep it to image and label
 rec_features = {
     'image': tf.io.FixedLenFeature([], tf.string),
     'label': tf.io.FixedLenFeature([], tf.int64)
 }
 
-###
-#FUNCTIONS TO DOWLOAD DATASET AND CREATE TFRECORDS
-###
 
-#download a file and write to disk  
-@timer
-def download_file(url, dst):
-    file_size = int(requests.head(url).headers["Content-Length"])
+#get filenames in image data and split them as train and eval set 
 
-    pbar = tqdm(
-        total=file_size, initial=0,
-        unit='B', unit_scale=True, desc=url.split('/')[-1])
-
-    req = requests.get(url, stream=True)
-    with(open(dst, 'ab')) as f:
-        for chunk in req.iter_content(chunk_size=10 * 1024):
-            if chunk:
-                f.write(chunk)
-                pbar.update(10 * 1024)
-    pbar.close()
-    return file_size
-
-#dowload cifar10 data
-@timer
-def download_cifar10_files():
-    path = './'
-    if tar not in os.listdir(path):
-        download_file(url, tar)
-    else:
-        print('dataset archive file exists!')
-
-#extract cifar 10 data files downloaded archive 
-@timer
-def extract_cifar10_files():
-    data = './cifar10_data/'
-    if os.path.exists(data + 'cifar-10-batches-py/test_batch'):
-        print(data + 'cifar-10-batches-py/', 'is not empty and has test_batch file!')
-
-    else:
-        tarfile.open(tar, 'r:gz').extractall(data)
-        print('Done')
-
-#get filenames in cifar 10 data and split them as train and eval set 
-@timer
 def _get_file_names():
     """Returns the file names expected to exist in the input_dir."""
     file_names = {}
-    file_names['train'] = ['data_batch_%d' % i for i in xrange(1, 6)]
-    file_names['eval'] = ['test_batch']
+    file_names['train'] = list(zip(train_df1.Filename, train_df1.Label))
+    file_names['eval'] = list(zip(val_df.Filename, val_df.Label))
+    file_names['test'] = list(zip(test_df.Filename, test_df.Label))
+    
     return file_names
-
-#read the data files 
-@timer
-def read_pickle_from_file(filename):
-    with tf.io.gfile.GFile(filename, 'rb') as f:
-        if sys.version_info >= (3, 0):
-            data_dict = pickle.load(f, encoding='bytes')
-        else:
-            data_dict = pickle.load(f)
-    return data_dict
-
 
 #tfrecord features for label and image in cifar 10 dataset 
 #label
@@ -126,53 +73,55 @@ def _bytes_feature(value):
 
 #convert downloaded data files to tfrecords 
 @timer
-def convert_to_tfrecord(input_files, output_file):
+def convert_to_tfrecord(input_files, output_file,images_dir=''):
     """Converts a file to TFRecords."""
     print('Generating %s' % output_file)
     with tf.io.TFRecordWriter(output_file) as record_writer:
-        for input_file in input_files:
-            # print(input_file)
-            data_dict = read_pickle_from_file(input_file)
-            data = data_dict[b'data']
-            labels = data_dict[b'labels']
-            num_entries_in_batch = len(labels)
-            # print(num_entries_in_batch)
+        for filename, label in input_files:
+            if images_dir !='':
+              filename=os.path.join(images_dir,filename)
+            img = Image.open(filename)
+            img = np.array(img.resize((256,256)))
 
-            for i in range(num_entries_in_batch):
-                example = tf.train.Example(features=tf.train.Features(
-                    feature={
-                        'image': _bytes_feature(data[i].tobytes()),
-                        'label': _int64_feature(labels[i])
-                    }))
-                record_writer.write(example.SerializeToString())
+            feature = { 'label': _int64_feature(label),
+                         'image': _bytes_feature(img.tostring()) }
+
+            # Create an example protocol buffer
+
+            example = tf.train.Example(features=tf.train.Features(feature=feature))
+            
+            
+            record_writer.write(example.SerializeToString())
 
 #function to check if tfrecords exist , else create them 
 @timer
-def create_tf_records(data_dir='./cifar10_data', output_dir='./', overwrite=False):
+def create_tf_records(data_dir='./data', output_dir='./tf_train',images_dir='./data', overwrite=False):
     file_names = _get_file_names()
-
-    input_dir = os.path.join(data_dir, 'cifar-10-batches-py')
+    if os.path.exists(output_dir):
+      pass
+    else:  
+      os.mkdir(output_dir)
+    input_dir = data_dir
     for mode, files in file_names.items():
-        input_files = [os.path.join(input_dir, f) for f in files]
-        output_file = mode + '.tfrecords'
-        if ((output_file in os.listdir(output_dir)) & (overwrite == False)):
-            print(output_file, 'exists!')
-        else:
-            output_file = os.path.join(output_dir, output_file)
-            try:
-                os.remove(output_file)
-            except OSError:
-                pass
+      
+      #for file,label in files:
+      #input_files =[os.path.join('./',f) for f in input_files]
+      output_file = mode + '.tfrecords'
+      if ((output_file in os.listdir(output_dir)) & (overwrite == False)):
+          print(output_file, 'exists!')
+      else:
+          output_file = os.path.join(output_dir, output_file)
+          try:
+              os.remove(output_file)
+          except OSError:
+              pass
             # Convert to tf.train.Example and write the to TFRecords.
-            convert_to_tfrecord(input_files, output_file)
-            print('Done!')
+          convert_to_tfrecord(files, output_file,images_dir)
+          print('Done!')
+        
+        
 
-# putting it all together -- download data and create tfrecords 
-@timer
-def get_cifar10_and_create_tfrecords():
-    download_cifar10_files()
-    extract_cifar10_files()
-    create_tf_records()
+
 
 ###
 #FUNCTIONS TO READ TFRECORDS AND CREATE DATASETS  
@@ -184,11 +133,13 @@ def _parse_record_function(im_example):
 
 #parse a batch of records if you batch before map
 def parse_batch(batch_of_records):
+    print('---parse batch -----')
     records=tf.io.parse_example(batch_of_records,rec_features)
     image = tf.io.decode_raw(records['image'], tf.uint8)
-    
-    #image.set_shape([batch_size * 32 * 32 * 3]) # refer to https://stackoverflow.com/questions/35451948/clarification-on-tf-tensor-set-shape
-    image=tf.transpose(tf.reshape(image,[batch_size,3,32,32]),[0,2,3,1])
+    print(image.dtype,image.shape,len(image))
+    #image.set_shape([batch_size * 224 * 224 * 3]) # refer to https://stackoverflow.com/questions/35451948/clarification-on-tf-tensor-set-shape
+    #image=tf.transpose(tf.reshape(image,[batch_size,3,224,224]),[0,2,3,1])
+    image=tf.reshape(image,[batch_size,256,256,3])
     #cast image as float32 as the model requires it
     image = tf.cast(image,tf.float32)
     #augment image if needed
@@ -206,14 +157,15 @@ def parse_batch_distort(batch_of_records):
     records=tf.io.parse_example(batch_of_records,rec_features)
     image = tf.io.decode_raw(records['image'], tf.uint8)
     
-    #image.set_shape([batch_size * 32 * 32 * 3]) # refer to https://stackoverflow.com/questions/35451948/clarification-on-tf-tensor-set-shape
-    image=tf.transpose(tf.reshape(image,[batch_size,3,32,32]),[0,2,3,1])
+    #image.set_shape([batch_size * 224 * 224 * 3]) # refer to https://stackoverflow.com/questions/35451948/clarification-on-tf-tensor-set-shape
+    #image=tf.transpose(tf.reshape(image,[batch_size,3,224,224]),[0,2,3,1])
+    image=tf.reshape(image,[batch_size,256,256,3])
     #cast image as float32 as the model requires it
     image = tf.cast(image,tf.float32)
     
     #image augmenation 
     print('distorting image')
-    image=aug1(image)
+    #image=aug1(image)
     
 
     label = tf.cast(records['label'], tf.int32)
@@ -229,8 +181,9 @@ def parse_record(im_example,distort,distort_fn):
     record = tf.io.parse_single_example(im_example, rec_features)
     
     image = tf.io.decode_raw(record['image'], tf.uint8)
-    image.set_shape([32 * 32 * 3])
-    image = tf.transpose(tf.reshape(image, [3, 32, 32]), [1, 2, 0])
+    image.set_shape([256 * 256 * 3])
+    #image = tf.transpose(tf.reshape(image, [3, 256, 256]), [1, 2, 0])
+    image = tf.reshape(image, [256, 256,3])
     #image = tf.reshape(image,[32,32,3]) #check this ..this doesn't seem to give the right image 
     image = tf.cast(image,tf.float32)
     #augment image if needed
@@ -238,10 +191,10 @@ def parse_record(im_example,distort,distort_fn):
       print('distorting...')
     
       image = distort_fn(image)
-
+    
     label = tf.cast(record['label'], tf.int32)
     label = tf.one_hot(label, num_classes)
-
+    
     return image, label
   
   
@@ -251,10 +204,12 @@ def parse_record(im_example,distort,distort_fn):
 def create_train_eval_datasets():
     train_records = tf.data.TFRecordDataset('./train.tfrecords')
     eval_records = tf.data.TFRecordDataset('./eval.tfrecords')
+    test_records = tf.data.TFRecordDataset('./test.tfrecords')
     train_dataset = train_records.map(_parse_record_function)
     eval_dataset = eval_records.map(_parse_record_function)
+    test_dataset = test_records.map(_parse_record_function)
 
-    return train_dataset, eval_dataset
+    return train_dataset, eval_dataset, test_dataset
 
 #function to create dataset from tfrecords
 @timer
@@ -266,7 +221,8 @@ def get_tf_dataset(recordsfile, batch_size, shuffle=False,distort=False):
   if shuffle:
     dataset = dataset.shuffle(buffer_size=10*batch_size)
   #repeat
-  dataset = dataset.repeat()
+  if recordsfile != './tf_train/test.tfrecords':
+    dataset = dataset.repeat()
   #batch before map is recommended for speed
   #refer https://stackoverflow.com/questions/50781373/using-feed-dict-is-more-than-5x-faster-than-using-dataset-api
   # batch the records
@@ -275,10 +231,10 @@ def get_tf_dataset(recordsfile, batch_size, shuffle=False,distort=False):
   #setting num_parallel_calls to a value much greater than the number of available CPUs 
   #can lead to inefficient scheduling, resulting in a slowdown
   if distort:
-    #print('distorting...')
-    dataset = dataset.map(map_func=parse_batch_distort,num_parallel_calls=CPU_CORES)
+    print('distorting...')
+    dataset = dataset.map(map_func=parse_batch_distort,num_parallel_calls=4)
   else:
-    dataset = dataset.map(map_func=parse_batch,num_parallel_calls=CPU_CORES)
+    dataset = dataset.map(map_func=parse_batch,num_parallel_calls=4)
   #prefetch elements from the input dataset ahead of the time they are requested
   dataset = dataset.prefetch(buffer_size=1)
   
@@ -299,7 +255,7 @@ def get_tf_dataset_2(recordsfile, batch_size, shuffle=False,distort=False, disto
   #parse the records - map to parse function
   #setting num_parallel_calls to a value much greater than the number of available CPUs 
   #can lead to inefficient scheduling, resulting in a slowdown
-  dataset = dataset.map(map_func=lambda record: parse_record(record, distort, distort_fn),num_parallel_calls=CPU_CORES)
+  dataset = dataset.map(map_func=lambda record: parse_record(record, distort, distort_fn),num_parallel_calls=4)
 
 
   #although the following link says batch before map is recommended for speed, some tf2 image functions don't seem to handle batches optimally
@@ -318,9 +274,11 @@ def get_tf_dataset_2(recordsfile, batch_size, shuffle=False,distort=False, disto
 def get_tf_dataset_in_batches(recordstype='train', batch_size=128, shuffle=False,distort=False,distort_fn=None):
   #switch file name based on train or test data
   if recordstype == 'train':
-    recordsfile = './train.tfrecords'
-  else:
-    recordsfile = './eval.tfrecords'
+    recordsfile = './tf_train/train.tfrecords'
+  elif recordstype== 'val':
+    recordsfile = './tf_train/eval.tfrecords'
+  elif recordstype== 'test':
+    recordsfile = './tf_train/test.tfrecords'  
   #create dataset 
   if distort_fn ==None:
     dataset = get_tf_dataset(recordsfile, batch_size,shuffle,distort)
@@ -336,13 +294,19 @@ def get_tf_dataset_in_batches(recordstype='train', batch_size=128, shuffle=False
 
 #create train data 
 @timer
-def get_train_ds(batch_size=128,shuffle=True,distort=True,distort_fn=None):
+def get_train_ds(batch_size=batch_size,shuffle=True,distort=True,distort_fn=aug1):
     train_ds = get_tf_dataset_in_batches('train', batch_size, shuffle,distort,distort_fn)
     return train_ds
   
   
+#create val data
+@timer
+def get_eval_ds(batch_size=batch_size):
+    val_ds = get_tf_dataset_in_batches('val', batch_size)
+    return val_ds
+
 #create test data
 @timer
-def get_eval_ds(batch_size=128):
+def get_test_ds(batch_size=batch_size):
     test_ds = get_tf_dataset_in_batches('test', batch_size)
-    return test_ds
+    return test_ds     
