de00b6a4d2aa8960be8b9d8c7058ad6b38d358c9
Ravindra Bharathi <ravindrabharathi@users.noreply.github.com>
2021-02-16 10:35:10 +0530

    Update data.py

--- tf_utils/data.py
+++ tf_utils/data.py
@@ -43,11 +43,16 @@ def get_train_val_test_df(data_df):
   train_df1, val_df = train_test_split(train_df, test_size=0.25,shuffle=True,random_state=42)
   return train_df1,val_df,test_df
 
-def get_class_map_and_species_names(data_df):
-  class_map=data_df[['Label','Species']]
-  class_map=class_map.drop_duplicates()
-  class_map=class_map.sort_values(by=['Label']).reset_index(drop=True)
-  species_names=class_map.to_dict()['Species']
+def get_train_val_df(data_df):
+  global train_df1,val_df
+  train_df, val_df = train_test_split(data_df, test_size=0.2,shuffle=True,random_state=42)
+  return train_df,val_df
+
+def get_class_map_and_species_names(map_json):
+  class_map=map_json
+  
+  
+  species_names=class_map
   return class_map,species_names
 
 
