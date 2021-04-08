c5f2c3c6bf7b0c23fb95e671cfdfcac5e1f71f30
Ravindra Bharathi <ravindrabharathi@users.noreply.github.com>
2021-04-08 14:50:26 +0530

    added confidence and margin sampling methods

--- tf_utils/data.py
+++ tf_utils/data.py
@@ -42,12 +42,16 @@ def split_train_test_df(data_df):
   global train_df, train_df1,test_df
   train_df, test_df = train_test_split(data_df, test_size=0.2,shuffle=True,random_state=42)
   train_df1,train_df=train_test_split(train_df, test_size=0.75,shuffle=True,random_state=42)
+  train_df=train_df.reset_index()
+  train_df1=train_df1.reset_index()
+  test_df=test_df.reset_index()
   return train_df1,train_df,test_df
 
 def get_random_train_df():
   global train_df,train_df1
   train_df2,train_df=train_test_split(train_df, test_size=0.75,shuffle=True,random_state=42)
   train_df1=pd.concat([train_df1, train_df2], axis=0).reset_index()
+  train_df=train_df.reset_index()
   return train_df1,train_df
 
 def get_train_val_test_df(data_df):
@@ -334,3 +338,86 @@ def get_unlabelled_ds(batch_size=batch_size):
 def get_test_ds(batch_size=batch_size):
     test_ds = get_tf_dataset_in_batches('test', batch_size)
     return test_ds     
+
+
+#get least_confidence_indices  
+def get_least_confidence_samples(model,ds,num_steps,total_size,sample_size):
+  if sample_size<=1:
+    sample_size=int(total_size*sample_size)
+  pred=model.predict(ds,num_steps=num_steps,verbose=0)
+  pred=pred[:total_size]
+  conf=[]
+  indices=[]
+  for idx,predxn in enumerate(pred):
+    conf.append(np.max(predxn))
+    indices.append(idx)
+  conf=np.asarray(conf)
+  indices=np.asarray(indices)
+  least_conf_indices=np.argsort(conf)[:sample_size]
+  
+  return least_conf_indices
+
+
+#add sample with least confident predictions to training dataset and change unlabelled ds accordingly
+def add_least_confidence_sample(model,ds,num_steps,total_size,sample_size):
+  global train_df, train_df1
+  if sample_size<=1:
+    sample_size=int(total_size*sample_size)
+  pred=model.predict(ds,num_steps=num_steps,verbose=0)
+  pred=pred[:total_size]
+  conf=[]
+  indices=[]
+  for idx,predxn in enumerate(pred):
+    conf.append(np.max(predxn))
+    indices.append(idx)
+  conf=np.asarray(conf)
+  indices=np.asarray(indices)
+  least_conf_indices=np.argsort(conf)[:sample_size]
+  train_df2=train_df.loc[least_conf_indices,:].reset_index()
+  train_df=train_df[~train_df.isin(train_df2)].dropna()
+  train_df.reset_index()
+  train_df1=pd.concat([train_df1, train_df2], axis=0).reset_index()
+  return train_df1, train_df
+
+
+#get top-2-confidence-margin indices  
+def get_top2_confidence_margin_samples(model,ds,num_steps,total_size,sample_size):
+  if sample_size<=1:
+    sample_size=int(total_size*sample_size)
+  pred=model.predict(ds,num_steps=num_steps,verbose=0)
+  pred=pred[:total_size]
+  margins=[]
+  indices=[]
+  for idx,predxn in enumerate(pred):
+    sorted_predxn=predxn.sort(reverse=True)
+    margins.append(sorted_predxn[0]-sorted_predxn[1])
+    indices.append(idx)
+  margins=np.asarray(margins)
+  indices=np.asarray(indices)
+  least_margin_indices=np.argsort(margins)[:sample_size]
+  
+  return least_margin_indices
+
+#add top-2-confidence-margin samples for training  
+def add_top2_confidence_margin_samples(model,ds,num_steps,total_size,sample_size):
+  global train_df, train_df1
+  if sample_size<=1:
+    sample_size=int(total_size*sample_size)
+  pred=model.predict(ds,num_steps=num_steps,verbose=0)
+  pred=pred[:total_size]
+  margins=[]
+  indices=[]
+  for idx,predxn in enumerate(pred):
+    sorted_predxn=predxn.sort(reverse=True)
+    margins.append(sorted_predxn[0]-sorted_predxn[1])
+    indices.append(idx)
+  margins=np.asarray(margins)
+  indices=np.asarray(indices)
+  least_margin_indices=np.argsort(margins)[:sample_size]
+  train_df2=train_df.loc[least_margin_indices,:].reset_index()
+  train_df=train_df[~train_df.isin(train_df2)].dropna()
+  train_df.reset_index()
+  train_df1=pd.concat([train_df1, train_df2], axis=0).reset_index()
+  
+  return train_df1, train_df
+  
